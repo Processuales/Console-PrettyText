bfe45fa0797819a0f88e3bb30bbe783f62d1211b
xMoonLike <vladheg77@gmail.com>
2023-10-31 22:37:38 -0400

    Added Main.cpp
    Base code. Rewrite to .h later

---
+++ Main.cpp
@@ -0,0 +1,268 @@
+#ifdef _WIN32
+#include <Windows.h>
+#define MY_SLEEP(time) Sleep(time * 1025 / 1000)
+#else
+#include <unistd.h>
+#define MY_SLEEP(time) usleep(time * 1025)
+#endif
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <cstring>
+#include <sstream>
+
+#define newl std::cout << "\n";
+constexpr bool IGNORE_SPACES = true;
+
+struct textDisplay
+{
+    std::string text;
+    std::vector<std::string> params = {};
+};
+
+std::unordered_map<std::string, std::string> colorMap = {
+    {"red", "31m"},    {"green", "32m"},  {"blue", "34m"}, {"black", "30m"},
+    {"yellow", "33m"}, {"purple", "35m"}, {"cyan", "36m"}, {"white", "37m"},
+};
+
+std::unordered_map<std::string, std::string> formattingParams = {
+    {"bold", "1;"},
+    {"underline", "4;"},
+    {"strikethrough", "9;"},
+};
+
+std::unordered_map<std::string, int> alignments = {
+    {"left-", 20},   {"left", 50},   {"left+", 80},   {"center", 100},
+    {"right-", 140}, {"right", 180}, {"right+", 200},
+};
+
+void console_print(const char& text, const std::string& fullcode = "")
+{
+    if (!fullcode.empty())
+    {
+        if (text != ' ')
+        {
+            std::cout << fullcode << text << "\033[0m" << std::flush;
+        } else
+        {
+            std::cout << text << std::flush;
+        }
+    } else
+    {
+	    std::cout << text << std::flush;
+    }
+    
+};
+
+void type_text(const std::string & text, unsigned int time = 0,
+    const std::vector<std::string>& params = {}) {
+
+	std::string color = "white";
+    std::string formattingCode = "\033[0;";
+    
+    int align = 0;
+    int paramCount = 0;
+    const unsigned int sleep_time = time / text.size();
+
+
+    for (const std::string& param : params) {
+        if (colorMap.find(param) != colorMap.end()) {
+            color = param;
+        }
+        if (formattingParams.find(param) != formattingParams.end()) {
+            formattingCode += formattingParams[param];
+            paramCount++;
+        }
+        if (alignments.find(param) != alignments.end()) {
+            align = alignments[param];
+        }
+    }
+
+    const std::string colorCode = colorMap[color];
+    const std::string fullCode = "\033[" + formattingCode + colorCode;
+
+    if (align != 0) {
+        std::cout << std::setw(align - text.size() / 2 + paramCount * 2);
+    }
+
+    for (std::size_t i = 0; i < text.size(); ++i) {
+        console_print(text[i], fullCode);
+        MY_SLEEP(sleep_time);
+    }
+}
+
+std::string get_full_code(const std::vector<std::string>& params)
+{
+    std::string color = "white";
+    std::string formattingCode = "\033[0;";
+
+    int paramCount = 0;
+
+    for (const std::string& param : params) {
+        if (colorMap.find(param) != colorMap.end()) {
+            color = param;
+        }
+        if (formattingParams.find(param) != formattingParams.end()) {
+            formattingCode += formattingParams[param];
+            paramCount++;
+        }
+    }
+
+    const std::string colorCode = colorMap[color];
+    const std::string fullCode = "\033[" + formattingCode + colorCode;
+    return fullCode;
+
+}
+
+unsigned int make_same_size(std::vector<textDisplay> &text_values)
+{
+    unsigned int iterations = 0;
+    unsigned int longest_text = 0;
+    for (textDisplay item : text_values)
+    {
+        if (iterations == 0)
+        {
+            longest_text = item.text.length();
+        }
+        else
+        {
+            if (item.text.length() > longest_text)
+            {
+                longest_text = item.text.length();
+            }
+        }
+        iterations++;
+    }
+
+    for (textDisplay &item : text_values)
+    {
+        const unsigned int diff_length = longest_text - item.text.length();
+	    if (diff_length != 0)
+	    {
+		    for (int i = 0; i < diff_length; i++)
+		    {
+                item.text += " ";
+		    }
+	    }
+    }
+    return longest_text;
+}
+
+std::vector<textDisplay> createDisplay(const textDisplay& display_main, const char* longString, std::vector<std::string> params = {}) {
+    if (std::strlen(longString) == 0) return {};
+
+    std::vector<textDisplay> resultDisplays;
+    std::stringstream ss(longString);
+    std::string line;
+
+    size_t index = 0;
+    while (std::getline(ss, line, '\n')) {
+        if (index < 1) {
+            textDisplay display = display_main;
+            display.text = line;
+            display.params = params;
+            resultDisplays.push_back(display);
+        }
+        else {
+            textDisplay display;
+            display.text = line;
+            display.params = params;
+            resultDisplays.push_back(display);
+        }
+        index++;
+    }
+
+
+    return resultDisplays;
+}
+
+
+void multi_text(std::vector<textDisplay> text_values, unsigned int time = 0)
+{
+    if (text_values.empty()) return;
+    if (text_values.size() == 1)
+    {
+        type_text(text_values[0].text, time, text_values[0].params);
+        return;
+    }
+
+	std::vector<std::string> full_code;
+    full_code.reserve(text_values.size());
+    const unsigned int text_len = make_same_size(text_values);
+
+    std::string color = "white";
+    std::string formattingCode = "\033[0;";
+    
+    const unsigned int sleep_time = time / text_len;
+
+
+    for (textDisplay &items : text_values)
+    {
+        full_code.push_back(get_full_code(items.params));
+    }
+
+
+    constexpr char esc(27);
+
+    const std::string linesAbove = std::to_string(text_values.size() - 1);
+    for (std::size_t i = 0; i < text_len; ++i) {
+        
+        
+        console_print(text_values[0].text[i], full_code[0]);
+
+        for (int j = 1; j < text_values.size(); j++)
+        {
+            std::cout << esc << "[1B";
+            std::cout << "\033[1D";
+            
+            console_print(text_values[j].text[i], full_code[j]);
+            
+        }
+
+        std::cout << esc << "[" + linesAbove + "A";
+        MY_SLEEP(sleep_time);
+    }
+    std::cout << esc << "[" + linesAbove + "B";
+}
+
+int main() {
+
+
+    textDisplay display1 = {"Hello World! This is the first phrase!", {"blue", "bold", "underline"}};
+    textDisplay display2 = { "Hello World! Second Phrase Here!", {"yellow", "bold", "underline", "strikethrough"}};
+    textDisplay display3 = { "Hello World! Wow, the third phrase is finally done!", {"purple", "underline"}};
+    textDisplay long_display;
+
+
+    const char* longString = R""""(                                                 
+        ,--,                                     
+      ,--.'|            ,--,    ,--,              
+   ,--,  | :          ,--.'|  ,--.'|             
+,---.'|  : '          |  | :  |  | :     ,---.   
+|   | : _' |          :  : '  :  : '    '   ,'\  
+:   : |.'  |   ,---.  |  ' |  |  ' |   /   /   | 
+|   ' '  ; :  /     \ '  | |  '  | |  .   ; ,. : 
+'   |  .'. | /    /  ||  | :  |  | :  '   | |: :
+|   | :  | '.    ' / |'  : |__'  : |__'   | .; : 
+'   : |  : ;'   ;   /||  | '.'|  | '.'|   :    |
+|   | '  ,/ '   |  / |;  :    ;  :    ;\   \  /  
+;   : ;--'  |   :    ||  ,   /|  ,   /  `----'   
+|   ,/       \   \  /  ---`-'  ---`-'            
+'---'         `----'                             
+)"""";
+
+
+
+    type_text("Lorem ipsum dolor sit amet", 750, { "red", "bold", "underline", "strikethrough"});
+    newl
+    multi_text({ display1,display2,display3 }, 950);
+    newl
+	multi_text(createDisplay(long_display, longString, {"green"}), 950);
+    newl
+	std::cout << "Tests have Finished!";
+
+    
+}
